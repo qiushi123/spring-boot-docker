398313637ff1d7466064469271877b996ef19bdb
qiushi123 <2501902696@qq.com>
2018-04-17 18:51:54 +0800

    忽略文件

--- src/main/java/com/neo/controller/HelloController.java
+++ src/main/java/com/neo/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String hello(Model model, @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         model.addAttribute("name", name);
         return "hello";

62b59e96948a53fc6f0b4595e6dd3c0709bd8d11
qiushi123 <2501902696@qq.com>
2018-04-20 12:16:18 +0800

    redis

--- src/main/java/com/neo/DockerApplication.java
+++ src/main/java/com/neo/DockerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching //开启redis缓存服务
 public class DockerApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
--- src/main/java/com/neo/controller/HelloController.java
+++ src/main/java/com/neo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.neo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,5 +14,15 @@ public class HelloController {
     public String hello(Model model, @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         model.addAttribute("name", name);
         return "hello";
+    }
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @RequestMapping("/redis")
+    public String redis(Model model) {
+        stringRedisTemplate.opsForValue().set("zzp", "big z");
+        model.addAttribute("redis", stringRedisTemplate.opsForValue().get("zzp"));
+        return "redis/redis";
     }
 }

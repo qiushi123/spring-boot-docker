cb8cc5cdf37aaf74df6038dd8de91a6f2c1e753f
qiushi123 <2501902696@qq.com>
2018-04-20 12:38:28 +0800

    redis设置iplist页的缓存

--- src/main/java/com/neo/service/impl/IPServiceImpl.java
+++ src/main/java/com/neo/service/impl/IPServiceImpl.java
@@ -5,10 +5,13 @@ import com.neo.repository.IPRepository;
 import com.neo.service.IPService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+// 设置redis
+@CacheConfig(cacheNames = "pvs")
 @Service
 public class IPServiceImpl implements IPService {
 
@@ -16,6 +19,7 @@ public class IPServiceImpl implements IPService {
     private IPRepository userRepository;
 
 
+    @CacheEvict(key = "#p0")
     @Override
     public List<Pv2048> getIPList() {
         return userRepository.findAll();
